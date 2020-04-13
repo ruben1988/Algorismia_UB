d88bc9400e40b43c64e65f32d46d682f7567faef
ruben1988 <https://ruben1988@github.com>
2020-04-13 13:13:12 +0200

    afegit ordenacio

---
+++ main.cc
@@ -0,0 +1,98 @@
+
+#include "ordenacio.h"
+#include <iostream>
+
+
+void ordenacio_examples(){
+
+    int choice;
+    ordenacio orde;
+    int T[6] = {7,3,4,5,6,1};
+
+    do
+    {
+        std::cout << std::endl
+             << " 1 - Ordenació per selecció.\n"
+             << " 2 - Ordenació per inserció.\n"
+             << " 4 - Help.\n"
+             << " 5 - Exit.\n"
+             << " Enter your choice and press return: ";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            //code 
+            std::cout << "Array sense ordenar: \n";
+            orde.print_array(6,T);
+            std::cout << "Array ordenat: \n";
+            orde.ordenacio_per_seleccio(6,T);
+            break;
+        case 2:
+            //code 
+            std::cout << "Array sense ordenar: \n";
+            orde.print_array(6,T);
+            std::cout << "Array ordenat: \n";
+            orde.ordenacio_per_insercio(6,T);
+            break;
+        case 3:
+            //code to make option for the game
+            break;
+        case 4:
+            //code to help the user like give him
+            //extra information about the mode and the controller
+            break;
+        case 5:
+            std::cout << "End of Program.\n";
+            break;
+        default:
+            std::cout << "Not a Valid Choice. \n"
+                 << "Choose again.\n";
+            break;
+        }
+
+    } while (choice != 5);
+}
+
+int main()
+{
+    int choice;
+
+    do
+    {
+        std::cout << std::endl
+             << " 1 - Exemples ordenació.\n"
+             << " 2 - Story.\n"
+             << " 4 - Help.\n"
+             << " 5 - Exit.\n"
+             << " Selecciona una opció i prem Enter: ";
+        std::cin >> choice;
+
+        switch (choice)
+        {
+        case 1:
+            //code 
+            ordenacio_examples();
+            break;
+        case 2:
+            //code to make score for this game to count how many times u win the game
+            break;
+        case 3:
+            //code to make option for the game
+            break;
+        case 4:
+            //code to help the user like give him
+            //extra information about the mode and the controller
+            break;
+        case 5:
+            std::cout << "End of Program.\n";
+            break;
+        default:
+            std::cout << "Not a Valid Choice. \n"
+                 << "Choose again.\n";
+            break;
+        }
+
+    } while (choice != 5);
+    return 0;
+}
---
+++ ordenacio.h
@@ -0,0 +1,58 @@
+#ifndef ORDENACIOH
+#define ORDENACIOH
+
+#include <iostream>
+
+class ordenacio{
+    public:
+        void ordenacio_per_seleccio(int n, int T[]);
+        void ordenacio_per_insercio(int n, int T[]);
+        void print_array(int len, int T[]);
+};
+
+void ordenacio::print_array(int len, int T[]){
+
+    for(int i = 0; i< len; i++){
+        std::cout << T[i] << ' ';
+    }
+    std::cout << "\n";
+}
+
+void ordenacio::ordenacio_per_insercio(int n, int T[]){
+    for (int i=1; i<n; i++){
+        int x = T[i];
+        int j = i - 1;
+        while (j>=0 && x<T[j]){
+            T[j+1] = T[j];
+            j = j - 1;
+        }
+        T[j+1] = x;
+
+    }
+    print_array(n, T);
+}
+
+void ordenacio::ordenacio_per_seleccio(int n, int T[]){    
+    
+    for(int i=0; i<=n-1; i++){
+        int minj = i;
+        int minx = T[i];
+        for (int j = i+1; j<n; j++){
+            if(T[j]<minx){
+                minj=j;
+                minx=T[j];
+            }
+
+        }
+    T[minj]=T[i];
+    T[i]=minx;
+    }
+    print_array(n, T);
+}
+
+
+
+
+
+
+#endif
